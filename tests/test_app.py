183990edf54f57e719cba4c300a5196a6a1c6663
sharafeddines <sas106@mail.aub.edu>
2024-11-12 17:03:51 +0200

    Added requirements.txt

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World"), "Hello, World!")
 
 if __name__ == "__main__":
     unittest.main()
